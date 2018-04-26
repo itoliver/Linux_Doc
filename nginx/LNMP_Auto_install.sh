6455970e9bf274b653295a3f0f4fa0da86e48510
itoliver <itoliver@qq.com>
2018-04-26 15:09:53 +0800

    nginx

--- nginx/LNMP_Auto_install.sh
+++ nginx/LNMP_Auto_install.sh
@@ -37,7 +37,7 @@ Type=forking
 PIDFile=/usr/local/nginx/logs/nginx.pid
 ExecStartPre=/usr/sbin/nginx -t
 ExecStart=/usr/sbin/nginx
-ExecReload=//usr/sbin/nginx -s reload
+ExecReload=/usr/sbin/nginx -s reload
 ExecStop=/usr/sbin/nginx -s stop
 PrivateTmp=true
  
